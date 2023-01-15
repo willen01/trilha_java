b10b81dbb952a66afc8ef2d327d313d600e99c65
willen <willen.sesantos@gmail.com>
2023-01-15 19:39:20 -0300

    Métodos

---
+++ _3metodos/src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    //sistema de smart tv
+    public static void main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+    }
+}
