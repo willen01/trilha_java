cbba3f23656acdcea7586f838ac1ed6e08ae8e74
willen <willen.sesantos@gmail.com>
2023-01-16 10:02:56 -0300

    chore: Terminal e argumento

---
+++ _6Terminal_e_argumentos/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        //para executar essa classe no terminal, acessamos seu diretório e executamos o comando java <nome_da_classe>
+        //eventuais modificações só serão aplicadas depois da compilação da classe
+        System.out.println("Oi turma, fui executado pelo terminal");
+    }
+}
