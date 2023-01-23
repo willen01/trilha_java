96a594ece02cb4e5b03ffd3ea4ec14bef656189b
willen <willen.sesantos@gmail.com>
2023-01-23 19:31:01 -0300

    chore: Exercicio de interface

---
+++ _5POO/_1POO/src/_02Heranca_associacao_interface/_04Interfaces/OperacaoMatematica.java
@@ -0,0 +1,8 @@
+package _02Heranca_associacao_interface._04Interfaces;
+
+public interface OperacaoMatematica {
+    void soma(double operando1, double operando2 );
+    void subtracao(double operando1, double operando2);
+    void multiplicacao(double operando1, double operando2);
+    void divisao(double operando1, double operando2);
+}
