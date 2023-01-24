187a2944b3ee09d46616e6c7f5ccf0b44a2c1bbc
willen <willen.sesantos@gmail.com>
2023-01-24 19:36:32 -0300

    chore: Conhecendo métodos List pt 2

--- _6Collections/_01Collections/src/ExemploLista/ExemploList.java
+++ _6Collections/_01Collections/src/ExemploLista/ExemploList.java
@@ -1,8 +1,6 @@
 package ExemploLista;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
@@ -17,7 +15,7 @@ public class ExemploList {
         System.out.println("Crie uma lista e adcione as sete notas");
 
         List<Double> notas = new ArrayList<Double>();
-        notas.add(7.0);
+        notas.add(3.0);
         notas.add(8.5);
         notas.add(9.0);
         notas.add(5.0);
@@ -41,6 +39,42 @@ public class ExemploList {
 
 //        exiba as notas na ordem informada
         System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
-        for (Double nota: notas ) System.out.println(nota);
+//        for (Double nota: notas ) System.out.println(nota);
+
+        System.out.println("Exiba a terceira nota adcionada: " + notas.get(2));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+//        exibe a soma de valores
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+
+        while (iterator.hasNext()) { // há um próximo elemento ? true or false
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma / notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista: ");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("A lista está vazia ? " + notas.isEmpty());
     }
 }
